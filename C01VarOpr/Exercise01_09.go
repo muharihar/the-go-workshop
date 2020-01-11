3ce6225f053987eb617de78be256f729bdf5b48a
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-01-11 11:22:11 +0700

    add c01 exercise 01.00

---
+++ C01VarOpr/Exercise01_09.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main(){
+	fmt.Println(os.Args[0])
+
+	// Main course
+	var total float64 = 2 * 13
+	fmt.Println("Sub :", total)
+
+	// Drinks
+	total = total + (4 * 2.25)
+	fmt.Println("Sub :", total)
+
+	// Discount
+	total = total - 5
+	fmt.Println("Sub :", total)
+
+	// 10% tips
+	tip := total * 0.1
+	fmt.Println("Tip :", tip)
+	total = total + tip
+	fmt.Println("Total :", total)
+
+	// Split bill
+	split := total / 2
+	fmt.Println("Split :", split)
+
+	// Reward every 5th visit
+	visitCount := 24
+	visitCount = visitCount + 1
+	remainder := visitCount % 5
+	if remainder == 0 {
+		fmt.Println("With this visit, you've earned a reward.")
+	}
+}
