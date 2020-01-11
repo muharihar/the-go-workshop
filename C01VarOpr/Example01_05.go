952964e5aa0e5c9c217063f4326209104d3378db
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-01-11 10:24:11 +0700

    add c01 example 01.05

---
+++ C01VarOpr/Example01_05.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// Type only
+	var start, middle, end float32
+	fmt.Println(start, middle, end)
+
+	// Initial value midex type
+	var name, left, right, top, bottom = "one", 1, 1.5, 2, 2.5;
+	fmt.Println(name, left, right, top, bottom)
+
+	// works with functions also
+	var Debug, LogLevel, startUpTime = getConfig()
+	fmt.Println(Debug, LogLevel, startUpTime)
+}
+
+func getConfig() (bool, string, time.Time) {
+	return false, "info", time.Now()
+}
