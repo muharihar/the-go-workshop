8b3626a33a1c421e392d0594749af5824ba18322
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-01-11 09:47:03 +0700

    add c01 activity 01.01

---
+++ C01VarOpr/Activity01_01.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstName := "Muhammad"
+	lastName := "Hari"
+	age := 33
+	peanutAllergy := false
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(age)
+	fmt.Println(peanutAllergy)
+}
