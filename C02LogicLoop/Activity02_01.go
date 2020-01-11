cd9dd6966d74f1c0934623b0c8202059a8d5042c
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-01-11 12:09:16 +0700

    add c02 activity 02.01

---
+++ C02LogicLoop/Activity02_01.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main(){
+	fmt.Println(os.Args[0])
+
+	for i := 1; i <= 100; i++ {
+		out := ""
+
+		if i%3 == 0 {
+			out += "Fizz"
+		}
+
+		if i%5 == 0 {
+			out += "Buzz"
+		}
+
+		if out == "" {
+			out = strconv.Itoa(i)
+		}
+
+		fmt.Println(out)
+	}
+}
