05f9a084ea4caf10cc6e293f30ef912765344b41
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-01-11 11:12:17 +0700

    add c01 exercise 01.06

---
+++ C01VarOpr/Exercise01_06.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func getConfig01_06()(bool, string, time.Time){
+	return false, "info", time.Now()
+}
+
+func main() {
+	fmt.Println(os.Args[0])
+
+	Debug01_06, LogLevel01_06, startUpTime01_06 := getConfig01_06()
+	fmt.Println(Debug01_06, LogLevel01_06, startUpTime01_06)
+}
