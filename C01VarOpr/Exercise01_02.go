79f0ce7ea2bfbce5cef030c4093cc0563144720d
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-01-11 10:57:23 +0700

    add c01 exercise 01.02

---
+++ C01VarOpr/Exercise01_02.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// Package level variable
+var foo string = "bar"
+
+func main() {
+	// Function level variable
+	var baz string = "qux"
+
+	fmt.Println(foo, baz)
+}
