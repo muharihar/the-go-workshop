f7629ee63c84a97042271a29ac91eafa2046012c
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-01-16 20:25:13 +0700

    add c04 exercise 04.08

---
+++ C04ComplexTypes/Exercise04_08.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println(os.Args[0])
+
+	if longest := findLongest(getPassedArd(3)); len(longest) > 0 {
+		fmt.Println("The longest word passed was: ", longest)
+	} else {
+		fmt.Println("There was an error")
+		os.Exit(1)
+	}
+}
+
+func getPassedArd(minArgs int) []string {
+	if len(os.Args) < minArgs {
+		fmt.Printf("At least %v arguments are needed\n", minArgs)
+		os.Exit(1)
+	}
+	var args []string
+	for i := 1; i < len(os.Args); i++ {
+		args = append(args, os.Args[i])
+	}
+	return args
+}
+
+func findLongest(args []string) string {
+	var longest string
+	for i := 0; i < len(args); i++ {
+		if len(args[i]) > len(longest) {
+			longest = args[i]
+		}
+	}
+	return longest
+}
