8eda756e5b36fd0b572e4fa24c6499d8acd198fb
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-01-22 06:28:54 +0700

    add c05 Exercise 05.02

---
+++ C05Functions/Exercise05_02.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println(os.Args[0])
+
+	hdr := []string{"empid", "employee", "address", "hours worked", "hourly rate", "manager"}
+	csvHdrCol(hdr)
+
+	hdr2 := []string{"employee", "empid", "hours worked", "address", "manager", "hourly rate"}
+	csvHdrCol(hdr2)
+}
+
+func csvHdrCol(header []string) {
+	csvHeadersToColumnIndex := make(map[int]string)
+	for i, v := range header {
+		v = strings.TrimSpace(v)
+		switch strings.ToLower(v) {
+		case "employee":
+			csvHeadersToColumnIndex[i] = v
+		case "hours worked":
+			csvHeadersToColumnIndex[i] = v
+		case "hourly rate":
+			csvHeadersToColumnIndex[i] = v
+		}
+	}
+	fmt.Println(csvHeadersToColumnIndex)
+}
---
+++ C05Functions/Exercise05_02_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestCsvHdrCol05_02(t *testing.T) {
+	//no need to test! ^_^
+}
