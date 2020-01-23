8b26032ab82ec944d2cb4fda4f0e12d54930bc80
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-01-23 07:08:12 +0700

    add c05 Exercise 05.08

---
+++ C05Functions/Exercise05_08.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println(os.Args[0])
+
+	devSalary := salary(50, 2080, developerSalary)
+	bossSalary := salary(150000, 25000, managerSalary)
+
+	fmt.Printf("Boss salary: %d\n", bossSalary)
+	fmt.Printf("Developer salary: %d\n", devSalary)
+}
+
+func salary(x, y int, f func(int, int) int) int {
+	pay := f(x, y)
+	return pay
+}
+
+func managerSalary(baseSalary, bonus int) int {
+	return baseSalary + bonus
+}
+
+func developerSalary(hourlyRate, hoursWorked int) int {
+	return hourlyRate * hoursWorked
+}
