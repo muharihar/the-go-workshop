2a17ff88ed20bd4e0417e10a8ac3935e8fa0ab41
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-01-26 05:36:42 +0700

    add c06 Example 06.00b - defer b

---
+++ C06Error/Example06_00b.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	a()
+	fmt.Println("This line will now get printed from main() function")
+}
+
+func a() {
+	b("good-bye")
+	fmt.Println("Back un function a()")
+}
+
+func b(msg string) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("error in func b()", r)
+		}
+	}()
+
+	if msg == "good-bye" {
+		panic(errors.New("something went wrong"))
+	}
+	fmt.Print(msg)
+}
---
+++ C06Error/Example06_00b_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestExample06_00b(t *testing.T) {
+	// no test available
+}
