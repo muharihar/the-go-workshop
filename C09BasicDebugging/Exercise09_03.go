3cc3cfca0acc3bb60d96c8a716520e53bb25e17a
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-01-31 19:10:21 +0700

    add c09 Exercise 09.03

---
+++ C09BasicDebugging/Exercise09_03.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type person struct {
+	lname  string
+	age    int
+	salary float64
+}
+
+func main() {
+	fmt.Println(os.Args[0])
+
+	fname := "Joe"
+	grades := []int{100, 87, 67}
+	states := map[string]string{"KY": "Kentucky", "WV": "West Virginia", "VA": "Virginia"}
+
+	p := person{lname: "Lincoln", age: 210, salary: 25000.00}
+
+	fmt.Printf("fname value %#v\n", fname)
+	fmt.Printf("grades value %#v\n", grades)
+	fmt.Printf("states value %#v\n", states)
+	fmt.Printf("p value %#v\n", p)
+}
---
+++ C09BasicDebugging/Exercise09_03_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestExercise09_03(t *testing.T) {
+	// test not available
+}
