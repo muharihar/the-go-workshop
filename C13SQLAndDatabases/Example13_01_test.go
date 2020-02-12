088704741c7068b10bf78bebca60cc4cc593cd68
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-02-12 07:10:39 +0700

    add c13 Example 13.01

---
+++ C13SQLAndDatabases/Example13_01_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestExample12_01(t *testing.T) {
+	t.Log("Test passing!")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/muharihar/the-go-workshop
 
 go 1.13
+
+require github.com/lib/pq v1.3.0
